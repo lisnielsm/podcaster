950f0b7bd971451b85afaf7658efa43614d469b0
Lisniel Sanchez <lsanchez@reservebar.com>
2026-01-13 13:04:47 +0100

    feat(start): create project with typescrit and webpack

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <h1>Hello from React with Webpack and TypeScript!</h1>
+      <p>
+        This is my React project, manually configured with native CSS and
+        TypeScript.
+      </p>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    liveReload: true,
+    watchFiles: ["src/**/*", "public/**/*"],
+  },
+};
